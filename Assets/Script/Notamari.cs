c830802beeb260eda152cc4674627c6cf17964ae
Ben Rog-Wilhelm <zorba-git20110501@pavlovian.net>
2015-09-10 02:15:09 -0700

    Do some fakery with physics to make the ball control better; handle the double-collide case properly

--- Assets/Script/Notamari.cs
+++ Assets/Script/Notamari.cs
@@ -7,6 +7,8 @@ public class Notamari : MonoBehaviour
     public Transform m_CameraAnchor;
 
     public float m_MovementTorque = 10f;
+    public float m_SphereAbsorption = 2f;
+    public float m_SphereExpansion = 2f;
 
     Rigidbody m_RigidBody;
 
@@ -21,17 +23,37 @@ public class Notamari : MonoBehaviour
     void OnCollisionEnter(Collision collision)
     {
         Debris debris = collision.gameObject.GetComponent<Debris>();
-        if (debris)
+        if (debris && debris.transform.parent != transform)
         {
             Debug.Log("Collided with debris!");
 
             // Wipe the debris' rigid body so it becomes part of us
-            Destroy(collision.gameObject.GetComponent<Rigidbody>());
+            Destroy(debris.GetComponent<Rigidbody>());
 
             // Make it a child of us
-            collision.gameObject.transform.SetParent(transform);
+            debris.transform.SetParent(transform);
 
-            // Now it's hard to move around!
+            // Now it's hard to move around! Move the sphere closer to our center, then expand it, in an attempt to flatten things out and make it easier to move around
+            SphereCollider debrisCollider = debris.GetComponent<SphereCollider>();
+            Assert.IsNotNull(debrisCollider);
+            if (debrisCollider)
+            {
+                // Deal with some annoying frames of references
+                // Take the debris's position in ballspace, move towards the center of the ball by (radius * sphereAbsorption)
+                // (we pick x out of the scale because it should be uniformly scaled)
+                Vector3 intendedCenter = debris.transform.localPosition - debris.transform.localPosition.normalized * debris.transform.localScale.x * m_SphereAbsorption;
+
+                // Now convert that point to worldspace
+                Vector3 intendedCenterWorld = transform.TransformPoint(intendedCenter);
+
+                // Now convert that point to debrisspace
+                debrisCollider.center = debris.transform.InverseTransformPoint(intendedCenterWorld);
+
+                // Finally, expand the sphere as planned
+                debrisCollider.radius *= m_SphereExpansion;
+
+                // There is probably a faster way to do this but whatever there's like fifty pieces of debris per level at most
+            }
 
             // Yay!
 
