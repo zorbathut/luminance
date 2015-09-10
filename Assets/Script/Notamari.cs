5df2ad881c796303b7b3cd28b0daa0b1cbdd3cb6
Ben Rog-Wilhelm <zorba-git20110501@pavlovian.net>
2015-09-09 23:59:16 -0700

    Basic movement

--- Assets/Script/Notamari.cs
+++ Assets/Script/Notamari.cs
@@ -1,18 +1,35 @@
 ﻿using UnityEngine;
+using UnityEngine.Assertions;
 using System.Collections;
 
 public class Notamari : MonoBehaviour
 {
+    public Transform m_CameraAnchor;
 
-    // Use this for initialization
+    public float m_MovementForce = 10f;
+
+    Rigidbody m_RigidBody;
+
     void Start()
     {
+        Assert.IsNotNull(m_CameraAnchor);
+
+        m_RigidBody = GetComponent<Rigidbody>();
+        Assert.IsNotNull(m_RigidBody);
+    }
         
+    void FixedUpdate()
+    {
+        m_RigidBody.AddForce(new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * m_MovementForce);
     }
 
-    // Update is called once per frame
     void Update()
     {
-
+        if (m_CameraAnchor)
+        {
+            // Move the camera along with the sphere; it's not a child of the sphere so we don't have to muck about with undoing rotations
+            m_CameraAnchor.transform.position = transform.position;
+        }
+        
     }
 }
