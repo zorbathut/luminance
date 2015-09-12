751c4ea9802c83a110bef283044408e758ffa2df
Ben Rog-Wilhelm <zorba-git20110501@pavlovian.net>
2015-09-12 05:07:14 -0700

    Tiny bit of polish - music no longer restarts when restarting the game :V

---
+++ Assets/Script/MusicManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Simple persistent singleton, nothin' fancy
+public class MusicManager : MonoBehaviour {
+    static MusicManager m_Instance = null;
+
+	void Start()
+    {
+        if (m_Instance)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            m_Instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+	}
+}
